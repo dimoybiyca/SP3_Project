3a64642f1292eb193f5bb483b0f2de331f2b0a38
dimoybiyca <xar4ak7even5@gmail.com>
2023-04-08 14:49:23 +0300

    code refactor and encapsulation

--- ng-desk_hardware/src/main.cpp
+++ ng-desk_hardware/src/main.cpp
@@ -2,111 +2,70 @@
 #include "Display.h"
 #include "StateManager.h"
 #include "Communicator.h"
-#include "Button.h"
-
-#define buttonJoyPin 5
-#define buttonStartPin 6
-#define buttonStopPin 7
+#include "Controls/JoyProcessor.h"
+#include "Controls/ButtonsProcessor.h"
 
 Display *display;
 StateManager *stateManager;
+Communicator *communicator;
 
-Communicator communicator;
-Button buttonStart;
-Button buttonStop;
-Button buttonJoy;
+JoyProcessor joyProcessor;
+ButtonsProcessor buttonsProcessor;
 
 unsigned long ButtonCnt = millis();
 unsigned long ListenerCnt = millis();
-int joy = 0;
-int size = 0;
-int offset = 0;
-
-bool first = true;
-int active = -1;
 
 void setup()
 {
-  communicator = Communicator();
-  communicator.initSerial(9600);
-
+  communicator = Communicator::getInstance();
   stateManager = StateManager::getInstance();
-
   display = Display::getInstance();
-  display->init();
+  joyProcessor = JoyProcessor();
+  buttonsProcessor = ButtonsProcessor();
 
-  buttonStart.init(buttonStartPin);
-  buttonStop.init(buttonStopPin);
-  buttonJoy.init(buttonJoyPin);
+  communicator->initSerial(9600);
+  display->init();
+  buttonsProcessor.initButtons();
 }
 
 void loop()
 {
 
+  // If there is no connection to PC, execute untill establish
   while (stateManager->getConnectionState() == State::DISCONNECTED)
   {
-    communicator.establishConnection();
-  }
-
-  if (millis() - ButtonCnt > 100)
-  {
-    if (buttonStop.isChanged())
-    {
-      bool state = buttonStop.getState();
-      if (state && active != -1)
-      {
-        Serial.print("lnch" + String(active));
-      }
-      else
-      {
-        if (active != -1)
-        {
-          Serial.print("rbt" + String(active));
-          active = -1;
-        }
-      }
-    }
-
-    int current = analogRead(A1);
-    if (current > 900 && joy != 1)
-    {
-      joy = 1;
-      if (offset < size - 1)
-      {
-        offset = offset + 1;
-      }
-    }
-    else if (current < 100 && joy != -1)
-    {
-      joy = -1;
-      if (offset > 0)
-      {
-        offset = offset - 1;
-      }
-    }
-    else if (current > 400 && current < 600)
-    {
-      joy = 0;
-    }
-
-    if (joy != 0 || stateManager->getListState() == State::LIST_CHANGED)
-    {
-      display->showList();
-    }
-
-    if (buttonJoy.isChanged())
-    {
-      active = offset;
-    }
-
-    ButtonCnt = millis();
+    communicator->establishConnection();
   }
 
   if (millis() - ListenerCnt > 60000 || stateManager->getListState() == State::LIST_EMPTHY)
   {
-    communicator.receiveList();
+    communicator->receiveList();
 
     ListenerCnt = millis();
-    first = false;
+  }
+
+  if (millis() - ButtonCnt > 100)
+  {
+    if (stateManager->getListState() != State::LIST_EMPTHY)
+    {
+      if (stateManager->getProjectState() != State::PROJECT_ACTIVE)
+      {
+        joyProcessor.process();
+      }
+
+      if (stateManager->getProjectState() == State::PROJECT_ACTIVE)
+      {
+        display->showActive();
+      }
+
+      if (stateManager->getListState() == State::LIST_CHANGED)
+      {
+        display->showList();
+      }
+
+      buttonsProcessor.process();
+    }
+
+    ButtonCnt = millis();
   }
 }
